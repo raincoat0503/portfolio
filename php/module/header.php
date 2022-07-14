c61458cc8da13753621f8e9e84f6b0a8e532f5b4
raincoat0503 <raincoat.503@gmail.com>
2022-07-14 12:10:13 +0900

    ファビコンの変更、ヘッダーの調整

--- php/module/header.php
+++ php/module/header.php
@@ -1,10 +1,20 @@
 <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="format-detection" content="telephone=no">
     <html lang=ja>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="shortcut icon" href="../../media/img/fabicon/favicon.ico">
-    <link rel="apple-touch-icon" href="../../media/img/fabicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" href="../../media/img/fabicon/android-chrome-192x192.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="http://makinokaito.main.jp/makinokaito.com/media/img/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="http://makinokaito.main.jp/makinokaito.com/media/img/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="http://makinokaito.main.jp/makinokaito.com/media/img/favicon/favicon-16x16.png">
+    <link rel="manifest" href="http://makinokaito.main.jp/makinokaito.com/media/img/favicon/site.webmanifest">
+    <link rel="mask-icon" href="http://makinokaito.main.jp/makinokaito.com/media/img/favicon/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+
+    <title>Makinokaito Portfolio</title>
 
     <link rel="stylesheet" href="https://use.typekit.net/irv4cot.css">
     <link href="https://use.fontawesome.com/releases/v6.1.1/css/all.css" rel="stylesheet">
@@ -13,17 +23,17 @@
 
 <body>
 
-<header class="header">
-    <div class="header_logo">
-        <h2 class="logo">MakinoKaito.</h2>
-        <p class="logo_text">designer</p>
-    </div>
-    <nav class="header_nav">
-        <ul class="header_list">
-            <li class="header_item"><a class="header_link header_link-underline" href="/index.php">HOME</a></li>
-            <li class="header_item"><a class="header_link header_link-underline" href="/php/page/works.php">WORKS</a></li>
-            <li class="header_item"><a class="header_link header_link-underline" href="/php/page/about.php">ABOUT</a></li>
-            <li class="header_item"><a class="header_link header_link-btn btn-black" href="/php/page/contact.php">CONTACT</a></li>
-        </ul>
-    </nav>
-</header>
+    <header class="header">
+        <div class="header_logo">
+            <h2 class="logo">MakinoKaito.</h2>
+            <p class="logo_text">designer</p>
+        </div>
+        <nav class="header_nav">
+            <ul class="header_list">
+                <li class="header_item"><a class="header_link header_link-underline" href="/index.php">HOME</a></li>
+                <li class="header_item"><a class="header_link header_link-underline" href="/php/page/works.php">WORKS</a></li>
+                <li class="header_item"><a class="header_link header_link-underline" href="/php/page/about.php">ABOUT</a></li>
+                <li class="header_item"><a class="header_link header_link-btn btn-black" href="/php/page/contact.php">CONTACT</a></li>
+            </ul>
+        </nav>
+    </header>
