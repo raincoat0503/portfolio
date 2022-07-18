c11d9a04a1a11d162a7c8814ad6d096cfc380880
raincoat0503 <raincoat.503@gmail.com>
2022-07-18 13:26:26 +0900

    メールフォーム完成？

--- php/module/check.php
+++ php/module/check.php
@@ -2,48 +2,25 @@
 
 <html>
 
-<p class="check_address"></p>
-<p class="check_fname"></p>
-<p class="check_lname"></p>
-<p class="check_text"></p>
-
 <?php
+
     mb_language("Japanese");
     mb_internal_encoding("UTF-8");
 
-    $to = $_POST['address'];
-    $title = $_POST['fname'];
-    $content = $_POST['lname'];
-    $text = $_POST['text'];
+    $from = "g3esportspingu@gmail.com";
+    $to = $_REQUEST['address'];
+    $fname = $_REQUEST['fname'];
+    $lname = $_REQUEST['lname'];
+    $massage = $_REQUEST['text'];
 
-    if (mb_send_mail($to, $title, $content.$text)) {
+    if (mb_send_mail($from,  $to, $fname,$lname, $massage)) {
         echo "メールを送信しました";
     } else {
         echo "メールの送信に失敗しました";
     }
+    echo $to;
     ?>
 
 </html>
 
 <?php include('../../php/module/footer.php') ?>
-
-<script>
-    // URLを取得
-    let url = new URL(window.location.href);
-
-    // URLSearchParamsオブジェクトを取得
-    let params = url.searchParams;
-
-    // getメソッド
-    console.log(params.get('address')); // address
-    console.log(params.get('fname')); // fname
-    console.log(params.get('lname')); // lname
-    console.log(params.get('text')); // text
-
-    $(function() {
-        $('.check_address').text(params.get('address'));
-        $('.check_fname').text(params.get('fname'));
-        $('.check_lname').text(params.get('lname'));
-        $('.check_text').text(params.get('text'));
-    });
-</script>
