105622434f72d250a80f07b2c6568d4594f0903b
raincoat0503 <raincoat.503@gmail.com>
2022-07-18 10:45:57 +0900

    メール送信システム

--- php/module/check.php
+++ php/module/check.php
@@ -7,27 +7,43 @@
 <p class="check_lname"></p>
 <p class="check_text"></p>
 
+<?php
+    mb_language("Japanese");
+    mb_internal_encoding("UTF-8");
+
+    $to = $_POST['address'];
+    $title = $_POST['fname'];
+    $content = $_POST['lname'];
+    $text = $_POST['text'];
+
+    if (mb_send_mail($to, $title, $content.$text)) {
+        echo "メールを送信しました";
+    } else {
+        echo "メールの送信に失敗しました";
+    }
+    ?>
+
 </html>
 
 <?php include('../../php/module/footer.php') ?>
 
 <script>
     // URLを取得
-let url = new URL(window.location.href);
+    let url = new URL(window.location.href);
 
-// URLSearchParamsオブジェクトを取得
-let params = url.searchParams;
+    // URLSearchParamsオブジェクトを取得
+    let params = url.searchParams;
 
-// getメソッド
-console.log(params.get('address')); // address
-console.log(params.get('fname')); // fname
-console.log(params.get('lname')); // lname
-console.log(params.get('text')); // text
+    // getメソッド
+    console.log(params.get('address')); // address
+    console.log(params.get('fname')); // fname
+    console.log(params.get('lname')); // lname
+    console.log(params.get('text')); // text
 
-$(function(){
-    $('.check_address').text(params.get('address'));
-    $('.check_fname').text(params.get('fname'));
-    $('.check_lname').text(params.get('lname'));
-    $('.check_text').text(params.get('text'));
-});
+    $(function() {
+        $('.check_address').text(params.get('address'));
+        $('.check_fname').text(params.get('fname'));
+        $('.check_lname').text(params.get('lname'));
+        $('.check_text').text(params.get('text'));
+    });
 </script>
--- php/page/contact.php
+++ php/page/contact.php
@@ -5,25 +5,25 @@
         <div class="contact_text">
             <p>Mail address</p>
         </div>
-        <input class="contact_form contact_form-mail" placeholder="Email address" type="text" name="address">
+        <input class="contact_form contact_form-mail" placeholder="Email address" type="text" name="address" method="POST">
     </div>
     <div class="contact_box">
         <div class="contact_text">
             <p>Fitst name</p>
         </div>
-        <input class="contact_form contact_form-fname" placeholder="First name" type="text" name="fname">
+        <input class="contact_form contact_form-fname" placeholder="First name" type="text" name="fname" method="POST">
     </div>
     <div class="contact_box">
         <div class="contact_text">
             <p>Last name</p>
         </div>
-        <input class="contact_form contact_form-lname" placeholder="Last name" type="text" name="lname">
+        <input class="contact_form contact_form-lname" placeholder="Last name" type="text" name="lname" method="POST">
     </div>
     <div class="contact_box">
         <div class="contact_text">
             <p>Contact us</p>
         </div>
-        <textarea class="contact_form contact_form-form" placeholder="Inquiry details" type="text" name="text"></textarea>
+        <textarea class="contact_form contact_form-form" placeholder="Inquiry details" type="text" name="text" method="POST"></textarea>
     </div>
     <div class="contact_box">
         <div class="contact_text">
